05d949c81a3e6a8bfaf745eff18011466b4ed889
Roshan <r.roshannadar@gmail.com>
2021-09-25 20:36:51 +0530

    add items

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,7 @@
-import { beanbags, shoes } from "./Items";
+import { beanbags, laptops, mobiles, shoes } from "./Items";
+import Helmet from "react-helmet";
+
+<Helmet bodyAttributes={{ style: "background-color : #EC3109" }} />;
 
 const Home = () => {
   const person = localStorage.getItem("who");
@@ -64,9 +67,64 @@ const Home = () => {
           ))}
         </div>
 
-        <h1 className="display-4 text-success">Bean bags</h1>
+        <h1 className="display-4 text-primary">Bean Bags</h1>
         <div className="d-flex flex-row overflow-auto flex-nowrap">
           {beanbags.map((data) => (
+            <div className="card col-3 p-2 text-center" style={boxstyle}>
+              <img className="card-img" src={data.pic} alt="shoe" />
+              <div className="card-body">
+                <h1 className="card-title text-primary">
+                  {data.model} {data.brand}
+                </h1>
+                <p className="card-text text-success">Colors {data.colors}</p>
+                <p className="card-text text-warning">Type {data.type}</p>
+                <p className="card-text text-danger">Price {data.cost}</p>
+                <input
+                  type="button"
+                  className="btn btn-danger float-start"
+                  onClick={() => wishing(data)}
+                  value="To wishlist"
+                />
+                <input
+                  type="button"
+                  className="btn btn-danger float-end"
+                  onClick={() => karting(data)}
+                  value="To kart"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <h1 className="display-4 text-success">Mobiles</h1>
+        <div className="d-flex flex-row overflow-auto flex-nowrap">
+          {mobiles.map((data) => (
+            <div className="card col-3 p-2 text-center" style={boxstyle}>
+              <img className="card-img" src={data.pic} alt="shoe" />
+              <div className="card-body">
+                <h1 className="card-title text-primary">{data.brand}</h1>
+                <p className="card-text text-success">Colors {data.size}</p>
+                <p className="card-text text-danger">Price {data.cost}</p>
+                <input
+                  type="button"
+                  className="btn btn-danger float-start"
+                  onClick={() => wishing(data)}
+                  value="To wishlist"
+                />
+                <input
+                  type="button"
+                  className="btn btn-danger float-end"
+                  onClick={() => karting(data)}
+                  value="To kart"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <h1 className="display-4 text-success">Laptops</h1>
+        <div className="d-flex flex-row overflow-auto flex-nowrap">
+          {laptops.map((data) => (
             <div className="card col-3 p-2 text-center" style={boxstyle}>
               <img className="card-img" src={data.pic} alt="shoe" />
               <div className="card-body">
--- src/Items.js
+++ src/Items.js
@@ -4,7 +4,7 @@ export const shoes = [
     brand: "puma",
     model: "idp",
     type: "sneakers",
-    colors: ["blue", "white", "black", "red"],
+    colors: "blue",
     cost: 1870,
   },
   {
@@ -12,7 +12,7 @@ export const shoes = [
     brand: "adidas",
     model: "leather",
     type: "casual",
-    colors: ["maroon", "black"],
+    colors: "blue",
     cost: 5301,
   },
   {
@@ -20,7 +20,7 @@ export const shoes = [
     brand: "puma",
     model: "pro",
     type: "formals",
-    colors: ["black", "red"],
+    colors: "White",
     cost: 2300,
   },
   {
@@ -28,7 +28,7 @@ export const shoes = [
     brand: "nike",
     model: "dash",
     type: "casual",
-    colors: ["blue", "white", "black", "red", "grey", "maroon", "green"],
+    colors: "white and black",
     cost: 2310,
   },
 ];
@@ -38,18 +38,84 @@ export const beanbags = [
     pic: "images/bag1.jfif",
     brand: "Syntel",
     size: [3, 2, 5],
+    colors: "blue",
     cost: 4500,
   },
   {
     pic: "images/bag2.jfif",
     brand: "FlipPro",
+    colors: "Dark Blue",
     size: [3, 2],
     cost: 3500,
   },
   {
     pic: "images/bag3.jfif",
     brand: "BagMass",
+    Colors: "Multiple colors",
     size: [4, 5],
     cost: 6500,
+  },
+];
+
+export const mobiles = [
+  {
+    pic: "images/mob1.jpg",
+    brand: "RedMi 5A",
+
+    size: ["Black"],
+    cost: 8500,
+  },
+
+  {
+    pic: "images/mob2.jpg",
+    brand: "One Plus",
+
+    size: ["Peach"],
+    cost: 45000,
+  },
+
+  {
+    pic: "images/mob3.jpg",
+    brand: "Poco X2",
+
+    size: ["Blue"],
+    cost: 15000,
+  },
+
+  {
+    pic: "images/mob4.jpg",
+    brand: "Samsung ",
+    size: ["Silver"],
+    cost: 1800,
+  },
+];
+
+export const laptops = [
+  {
+    pic: "images/lap1.jpg",
+    brand: "Legion",
+    size: ["Black"],
+    cost: 75000,
+  },
+
+  {
+    pic: "images/lap2.jpg",
+    brand: "Acer",
+    size: ["Black"],
+    cost: 46500,
+  },
+
+  {
+    pic: "images/lap3.jpg",
+    brand: "HP",
+    size: ["White"],
+    cost: 80000,
+  },
+
+  {
+    pic: "images/lap4.jpg",
+    brand: "DELL ",
+    size: ["White"],
+    cost: 65450,
   },
 ];
